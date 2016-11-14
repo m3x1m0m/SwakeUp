3dacdad091523c19bfe53fe657cf2dc5def507e2
elamre <elmarblabla@hotmail.com>
2016-11-14 02:02:13 +0100

    first commit of the timer, needs a lot of work. Is a 1 second timer now

---
+++ xmega/code/drivers/host/timer.c
@@ -0,0 +1,34 @@
+/*
+ * timer.c
+ *
+ * Created: 11/14/2016 1:11:06 AM
+ *  Author: elmar
+ */ 
+
+
+ #include <avr/interrupt.h>
+ #include "timer.h"
+
+ EVENT_REGISTER(EVENT_TIMER_1_HZ, "1 second pulse");
+
+ ISR(TIMER1_COMPA_vect) {
+	 event_fire(&EVENT_TIMER_1_HZ, 0);
+ }
+
+ static uint8_t init(void){   
+     // set compare match register to desired timer count:
+     OCR1A = 976;	//  1s / 1 / 1000000hZ / 1024prescaler
+     // turn on CTC mode:
+     TCCR1B |= (1 << WGM12);
+     // Set CS10 and CS12 bits for 1024 prescaler:
+     TCCR1B |= (1 << CS10);
+     TCCR1B |= (1 << CS12);
+     // enable timer compare interrupt:
+     TIMSK1 |= (1 << OCIE1A); 
+ }
+
+ static uint8_t deinit(void){
+	
+ }
+
+ MODULE_DEFINE(TIMER,"Timer", init, deinit);
---
+++ xmega/code/drivers/host/timer.h
@@ -0,0 +1,18 @@
+/*
+ * timer.h
+ *
+ * Created: 11/14/2016 1:11:16 AM
+ *  Author: elmar
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include "../../util/event.h"
+#include "../../util/module.h"
+
+EVENT_EXP(EVENT_TIMER_1_HZ);
+MODULE_EXP(TIMER);
+
+#endif /* TIMER_H_ */
