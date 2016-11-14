56407b72ae983915e74a407938de38095792bb1f
elamre <elmarblabla@hotmail.com>
2016-11-14 02:03:40 +0100

    Small example

--- xmega/code/main.c
+++ xmega/code/main.c
@@ -6,43 +6,43 @@
  */ 
 
  #define F_CPU 1000000UL
-
+ 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "util/module.h"
 #include "util/event.h"
+
 #include "drivers/host/uart.h"	//Breach of layering 
+#include "drivers/host/timer.h" //Breach of layering 
+
 #include "modules/screen.h"
 #include "pin_definitions.h"
 
-EVENT_REGISTER(EVENT_COUNT,"counter event");
+static void callback(Event * event, uint8_t * data);
+
+const static char strin[6] = "Hallo\n";
 
 int main(void) {
 	SSD1306_PORT |=  SSD1306_CS  | SSD1306_DC | (1<<2);
-	uart_init(B9600);
-	module_init(&Screen);
-		
-// 	write_command(SSD1306_COLUMN_ADDR);
-// 	write_command(0x00);
-// 	write_command(0x7F);
-// 	write_command(SSD1306_PAGE_ADDR);
-// 	write_command(0x00);
-// 	write_command(0x07);
-	//write_datas(&screenBuf,SCREEN_WIDTH*SCREEN_HEIGHT);
-    /* Replace with your application code */	 
-	
-	_delay_ms(1000);
-	char strin[6] = "Hallo\n";
-	uint16_t i = 0;
-    while (1) {
-		i++;
-		_delay_ms(1000);
-		if(i==10){
-			//event_fire(EVENT_COUNT, (uint8_t*) 0);
-		}
-		uart_write_blocked(&strin,6); 
-		//event_process();
+	module_init(&UART);
+	module_init(&TIMER);
+	//module_init(&Screen);	 
+	sei();
+	uart_job(strin,sizeof(strin),0); 
+	event_addListener(&EVENT_UART_JOB, callback);
+	event_addListener(&EVENT_TIMER_1_HZ, callback);
+    while (1) { 
+		event_process();
     }
 }
+ 
 
+static void callback(Event * event, uint8_t * data){
+	if(event == &EVENT_UART_JOB){
+		//toggle led
+	}else if (event == &EVENT_TIMER_1_HZ){
+		uart_job(strin,sizeof(strin),0);
+	}
+}
