f5f77b6b76e539b60d5baf77d7ec9d506a775166
elamre <elmarblabla@hotmail.com>
2017-01-29 16:15:15 +0100

    this main

--- xmega/code/main.c
+++ xmega/code/main.c
@@ -4,18 +4,9 @@
  * Created: 10/31/2016 3:44:36 PM
  * Author : elmar
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 #define F_CPU 16000000UL
 
-=======
->>>>>>> 6d2f0f26b4681ce73fd67bab23541d0e9b96cab4
-=======
-
-#define EVENT_SUPPORTS_SLEEP
-
->>>>>>> 716e6817f8e8dac007977d67b9ed2391bef4f304
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include "pin_definitions.h"
@@ -25,15 +16,8 @@
 
 #include "util/module.h"
 #include "util/event.h"
-
+#include "drivers/host/uart.h"
 #include "modules/log.h"
-#include "drivers/uart/terminal.h"
-
-#include "drivers/host/uart.h"  //Breach of layering 
-#include "drivers/host/timer.h" //Breach of layering 
-
-#include "modules/screen.h"
-
 
 #include "drivers/host/pwm.h"
 
@@ -42,50 +26,52 @@ static void callback(Event * event, uint8_t * data);
 LOG_INIT("Main");
 
 static void sleep(void) {
-    LED_PORT = LED_PORT & ~LED_ERROR;
-    sleep_cpu();
+    // sleep_cpu();
 }
 
 static void wakeUp(void) {
-    LED_PORT = LED_PORT | LED_ERROR;
-    sleep_disable();
+    //sleep_disable();
     //ADCSRA = adcsra;
 }
 
+void switchExternalCrystal_16mHz(void) {
+    //16MHz external crystal
+    OSC_XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_XTAL_16KCLK_gc;
+    //Enable external oscillator
+    OSC_CTRL |= OSC_XOSCEN_bm;
+    //Wait for clock stabilization
+    while (!(OSC_STATUS & OSC_XOSCRDY_bm));
+    // Selects clock system as external clock
+    // through change protection mechanism
+    CCP = CCP_IOREG_gc;
+    CLK_CTRL = CLK_SCLKSEL_XOSC_gc;
+}
+static char * name = "elmar";
+static char * name1 = "ELMAR";
 int main(void) {
-#ifdef EVENT_SUPPORTS_SLEEP
-    event_init(sleep, wakeUp);
-    sleep_enable();
-    set_sleep_mode(SLEEP_MODE_IDLE);
-#endif
-    LED_PORT        = 0xFF;
-    SSD1306_PORT    |=  SSD1306_CS  | SSD1306_DC | (1 << 2);
-<<<<<<< HEAD
-    LED_DDRD        |= LED_ERROR | LED_SOL;
+    switchExternalCrystal_16mHz();
+    LED_PORT.DIR = LED_PIN;
+    LED_PORT.OUTTGL = LED_PIN;
     module_init(&UART);
-    //module_init(&TIMER);
-    module_init(&PWM);
-=======
-    LED_DDRD        |=  LED_ERROR   | LED_SOL;
-    module_init(&LOGGER);
-    module_init(&TIMER);
-    event_addListener(&EVENT_TIMER_1_HZ, callback);
->>>>>>> 716e6817f8e8dac007977d67b9ed2391bef4f304
+#ifdef EVENT_SUPPORTS_SLEEP
+    //event_init(sleep, wakeUp);
+    //sleep_enable();
+    //set_sleep_mode(SLEEP_MODE_IDLE);
+#endif
     //module_init(&Screen);
+    PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm; //peripheral enable - interrupt levels (ALL)
     sei();
+    uart_job(name, 5, 0, &CP_PORT);
     while (1) {
+        //uart_writes_blocked("Elmar", 5, &CP_PORT);
         event_process();
     }
 }
 
 static void callback(Event * event, uint8_t * data) {
     static uint8_t i = 0;
-    if (event == &EVENT_TIMER_1_HZ) {
-        LOG_DEBUG("Timer event %d", i++);
-        LED_PORT = LED_PORT ^ LED_SOL;
-    }
+//     if (event == &EVENT_TIMER_1_HZ) {
+//         LOG_DEBUG("Timer event %d", i++);
+//         LED_PORT = LED_PORT ^ LED_SOL;
+//     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 716e6817f8e8dac007977d67b9ed2391bef4f304
