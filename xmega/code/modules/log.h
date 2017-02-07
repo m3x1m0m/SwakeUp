60e35b2baa866ba5d54a1d828bd9f85ca30b9be6
elamre <elmarblabla@hotmail.com>
2017-02-08 00:43:37 +0100

    Overhead that wasnt needed

--- xmega/code/modules/log.h
+++ xmega/code/modules/log.h
@@ -20,17 +20,22 @@ const static char _system[] =   "Sys";
 const static char _warning[] =  "War";
 const static char _error[] =    "Err";
 
-#define LOG_INFO(MSG, ...)          LOG_INTERNAL(_info, MSG, 0, ##__VA_ARGS__)
-#define LOG_DEBUG(MSG, ...)         LOG_INTERNAL(_debug, MSG, 0, ##__VA_ARGS__)
-#define LOG_SYSTEM(MSG, ...)        LOG_INTERNAL(_system, MSG, 0, ##__VA_ARGS__)
-#define LOG_WARNING(MSG, ...)       LOG_INTERNAL(_warning, MSG, 0, ##__VA_ARGS__)
-#define LOG_ERROR(MSG, ...)         LOG_INTERNAL(_error, MSG, 1, ##__VA_ARGS__)
+#define LOG_INFO(MSG, ...)          LOG_INTERNAL(_info, MSG, ##__VA_ARGS__)
+#define LOG_DEBUG(MSG, ...)         LOG_INTERNAL(_debug, MSG, ##__VA_ARGS__)
+#define LOG_SYSTEM(MSG, ...)        LOG_INTERNAL(_system, MSG, ##__VA_ARGS__)
+#define LOG_WARNING(MSG, ...)       LOG_INTERNAL(_warning, MSG, ##__VA_ARGS__)
+#define LOG_ERROR(MSG, ...)         LOG_INTERNAL_ERR(_error, MSG, ##__VA_ARGS__)
 
-#define LOG_INTERNAL(LEVEL, MSG, BLOCK, ...)\
+#define LOG_INTERNAL(LEVEL, MSG, ...)\
+        log_message("[%s][%s]%s:%d ",LEVEL,log_name,__FILE__,__LINE__); \
+        log_message(MSG, ##__VA_ARGS__);\
+        log_message("\r\n")
+
+#define LOG_INTERNAL_ERR(LEVEL, MSG, ...)\
         log_message("[%s][%s]%s:%d ",LEVEL,log_name,__FILE__,__LINE__); \
         log_message(MSG, ##__VA_ARGS__);\
         log_message("\r\n");\
-        if(BLOCK) log_error()
+        log_error()
 
 void log_message(const char * format, ...);
 void log_error(void);
