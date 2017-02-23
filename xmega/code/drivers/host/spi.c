c1940475e7fad8456a56380616c97544142d9f52
elamre <elmarblabla@hotmail.com>
2017-02-23 22:33:48 +0100

    In the works, does not work yet

--- xmega/code/drivers/host/spi.c
+++ xmega/code/drivers/host/spi.c
@@ -1,78 +1,31 @@
 /*
  * spi.c
  *
- * Created: 11/7/2016 10:59:54 AM
+ * Created: 2/1/2017 5:25:38 PM
  *  Author: elmar
  */
-
-#include <avr/io.h>
 #include "spi.h"
-#include "../../util/job.h"
+#include <avr/io.h>
 #include "../../pin_definitions.h"
+#include "../../modules/log.h"
 
-// #include <avr/interrupt.h>
+LOG_INIT("SPI");
 
-JOB_BUFFER_INIT(SpiBuffer, SPI_MAX_JOBS);
+void spiWriteByte(uint8_t byte) {
+    SPIC.DATA = byte;
+    //while ((SPIC.STATUS & SPI_IF_bm) == 0x00);
+}
 
-#define SPI_SPCR_ENABLE (1<<SPE)
-#define SPI_SPCR_MASTER (1<<MSTR)
-#define SPI_SPCR_FCK_16 (1<<SPR0)
-
-EVENT_REGISTER(SPI_JOB_FINISHED, "Reading or writing");
-
-
-//static CircularBuffer outBuffer;
-//static CircularBuffer inBuffer;
-//static volatile uint8_t transmitting = 0;
-
-
-uint8_t spi_init(void) {
-    /* Initialize the output buffer */
-    // circularBuffer_init(&outBuffer);
-    /* Set MOSI, SCK as Output */
-    // TODO SPI_PORT |= SPI_DDRB_MOSI | SPI_DDRB_SCK | SPI_DDRB_SS;
-    /* Enable SPI, Master, set clock rate fck/16 */
-    // TODO SPCR = SPI_SPCR_ENABLE | SPI_SPCR_MASTER | SPI_SPCR_FCK_16;
+static uint8_t init(void) {
+    //1101 0000
+    SPIC.CTRL = SPI_MODE_0_gc | SPI_PRESCALER_DIV16_gc | SPI_ENABLE_bm | SPI_MASTER_bm;    // SPI master, clock idle low, data setup on trailing edge, data sampled on leading edge, double speed mode enabled
+    SPIC.INTCTRL = 0x00; // ensure SPI interrupts are disabled
+    LOG_SYSTEM("SPI initialized");
     return 1;
 }
 
-uint8_t spi_deinit(void) {
-    return 1;
-}
-/*
-int8_t spi_write(uint8_t data){
-if(!transmitting){
-   SPDR = data;
-   transmitting = 1;
-   return 1;
-}else {
-   return circularBuffer_writeByte(data);
-}
-}
-
-uint8_t spi_writes(uint8_t * datas, uint8_t len){
-return circularBuffer_write(&outBuffer,datas,len);
-}*/
-uint8_t spi_writes(uint8_t * datas, uint8_t len) {
-    if (job_add(&SpiBuffer, datas, len, 0) == 0) {
-        //we are full
-        return 0;
-    }
-    //Todo enable interrupts
+static uint8_t deinit(void) {
     return 1;
 }
 
-void spi_write_blocking(uint8_t data) {
-    SPDR = data;
-    while (!(SPSR & (1 << SPIF)));
-}
-
-
-// SPI Transmission/reception complete ISR
-/*
-ISR(SPI_STC_vect) {
-
-}
-*/
-
-MODULE_DEFINE(SPI, "SPI", spi_init, spi_deinit);
+MODULE_DEFINE(SPI, "SPI", init, deinit);
