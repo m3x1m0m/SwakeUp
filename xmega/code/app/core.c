f2fd834622b2e5b0e5d3b085e1656464bdf7faed
elamre <elmarblabla@hotmail.com>
2017-03-01 15:23:08 +0100

    Start of a general core to distribute messages

---
+++ xmega/code/app/core.c
@@ -0,0 +1,12 @@
+/*
+ * core.c
+ *
+ * Created: 3/1/2017 2:59:44 PM
+ *  Author: elmar
+ */
+
+#include "core.h"
+
+EVENT_REGISTER(TIME_CHANGE, "Change of net time");
+EVENT_REGISTER(WEATHER_CHANGE, "Change of weather");
+EVENT_REGISTER(TIME_SOCIAL_MEDIACHANGE, "Social Media");
---
+++ xmega/code/app/core.h
@@ -0,0 +1,20 @@
+/*
+ * core.h
+ *
+ * Created: 3/1/2017 12:29:48 AM
+ *  Author: elmar
+ */
+
+
+#ifndef CORE_H_
+#define CORE_H_
+
+#include "../util/event.h"
+
+EVENT_EXP(TIME_CHANGE);
+EVENT_EXP(WEATHER_CHANGE);
+EVENT_EXP(SOCIAL_MEDIA);
+
+
+
+#endif /* CORE_H_ */
