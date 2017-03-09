cf298a851dac6ddae8ddd63b37ec1c2b79b9eb50
elamre <elmarblabla@hotmail.com>
2017-03-09 11:39:01 +0100

    if we ever get to use 8 bits per color, this is fixed now

--- xmega/code/modules/screen.h
+++ xmega/code/modules/screen.h
@@ -13,7 +13,7 @@
 #include "../util/image.h"
 
 #define COLOR_TO656(r,g,b)((r>>3)<<11)|((g>>2)<<5)|(b>>3)
-#define COLOR_TO888(r,g,b)((r<<3)<<16)|((g<<2)<<8)|(b<<3)
+#define COLOR_TO888(r,g,b)((r)<<16)|((g)<<8)|(b)
 
 enum SCREEN_SHAPE_TYPE {
     POINT, FILLED, LINE
