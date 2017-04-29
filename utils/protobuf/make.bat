1546423ca331dc44aef1e2763c3291cc6dc2995b
elamre <elmarblabla@hotmail.com>
2017-04-29 19:16:31 +0200

    Updated with the updated esp project

--- utils/protobuf/make.bat
+++ utils/protobuf/make.bat
@@ -1,2 +1,2 @@
-"generator\protoc.exe" --nanopb_out=../../code/xmega/util/protobuf --nanopb_out=../../code/esp/Basic_Serial/app --java_out=../../code/backend/src/main/java/com/swakeup/common swakeup.proto
+"generator\protoc.exe" --nanopb_out=../../code/xmega/util/protobuf --nanopb_out=../../code/esp/app --java_out=../../code/backend/src/main/java/com/swakeup/common swakeup.proto
 
