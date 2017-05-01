e5da1ab1376dbe37dc6888e839c26d9947fbad79
elamre <elmarblabla@hotmail.com>
2017-05-01 13:29:52 +0200

    start of wifi stream

---
+++ code/esp/app/StreamWifi.cpp
@@ -0,0 +1,41 @@
+/*
+ * StreamWifi.cpp
+ *
+ *  Created on: Apr 29, 2017
+ *      Author: elmar
+ */
+
+#include "StreamWifi.h"
+
+static StreamWifi wifiStream;
+
+static bool writeSerial(pb_ostream_t * stream __attribute__ ((unused)),
+		const pb_byte_t *buf, size_t count) {
+	return wifiStream.writeBytes((char*) buf, count);
+}
+
+StreamWifi::StreamWifi() :
+		ProtoStream(&writeSerial), client(
+				TcpClientDataDelegate(StreamWifi::onReceive, this)) {
+}
+
+bool StreamWifi::onReceive(TcpClient& client, char *data, int size) {
+	int i = 0;
+	for (; i < size; i++) {
+		processByte(data[i]);
+	}
+	return true;
+}
+
+bool StreamWifi::writeByte(const char byte) {
+	return client.write(&byte, 1, 0) == 1;
+}
+
+bool StreamWifi::writeBytes(const char * byte, int len) {
+	return client.write(byte, len, 0) == len;
+}
+
+StreamWifi::~StreamWifi() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ code/esp/include/StreamWifi.h
@@ -0,0 +1,28 @@
+/*
+ * StreamWifi.h
+ *
+ *  Created on: Apr 29, 2017
+ *      Author: elmar
+ */
+
+#ifndef APP_STREAMWIFI_H_
+#define APP_STREAMWIFI_H_
+
+#include "ProtoStream.h"
+#include <string.h>
+
+class StreamWifi : public ProtoStream {
+public:
+	StreamWifi();
+	bool connect(String ssid, String pass);
+	bool writeByte(const char byte);
+	bool writeBytes(const char * byte, int len);
+	bool onReceive(TcpClient& client, char *data, int size);
+	virtual ~StreamWifi();
+private:
+	TcpClient client;
+};
+
+extern StreamWifi wifiStream;
+
+#endif /* APP_STREAMWIFI_H_ */
