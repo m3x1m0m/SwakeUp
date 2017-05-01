5f258a50cdebbb668908a326f25a5cae2a8ec8f8
elamre <elmarblabla@hotmail.com>
2017-05-02 00:14:00 +0200

    updated java package, updated to use with tools

--- utils/protobuf/make.bat
+++ utils/protobuf/make.bat
@@ -1,2 +1,2 @@
-"generator\protoc.exe" --nanopb_out=../../code/xmega/util/protobuf --nanopb_out=../../code/esp/app --java_out=../../code/backend/src/main/java/com/swakeup/common swakeup.proto
+"generator\protoc.exe" --nanopb_out=../../code/xmega/util/protobuf --nanopb_out=../../code/esp/app --java_out=../../code/backend/src/main/java/com/swakeup/common --java_out=../ConnTest/src/main/java/com/swakeup/common swakeup.proto
 
