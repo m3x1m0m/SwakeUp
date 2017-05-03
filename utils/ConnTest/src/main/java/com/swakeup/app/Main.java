45e1bab82c9455a5734c1f79167dc38106ad9a8e
elamre <elmarblabla@hotmail.com>
2017-05-03 18:27:43 +0200

    Some cleaning up

--- utils/ConnTest/src/main/java/com/swakeup/app/Main.java
+++ utils/ConnTest/src/main/java/com/swakeup/app/Main.java
@@ -40,8 +40,6 @@ public class Main {
         con.setRequestProperty("User-Agent", USER_AGENT);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-
         // Send post request
         con.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
@@ -58,7 +56,6 @@ public class Main {
 
         int responseCode = con.getResponseCode();
         System.out.println("\nSending 'POST' request to URL : " + PostPoint);
-        System.out.println("Post parameters : " + urlParameters);
         System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
