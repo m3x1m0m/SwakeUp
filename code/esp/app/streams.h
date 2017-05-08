f2b080fd722a5f4b7bfb0621d4482aba6e8c3aa6
elamre <elmarblabla@hotmail.com>
2017-05-08 21:31:27 +0200

    Start of xmega integration

---
+++ code/esp/app/streams.h
@@ -0,0 +1,14 @@
+/*
+ * streams.h
+ *
+ *  Created on: May 8, 2017
+ *      Author: elmar
+ */
+
+#ifndef APP_STREAMS_H_
+#define APP_STREAMS_H_
+
+#include "StreamRest.h"
+#include "StreamXmega.h"
+
+#endif /* APP_STREAMS_H_ */
